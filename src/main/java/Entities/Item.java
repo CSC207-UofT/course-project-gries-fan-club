7ef68c8adfc0408ee379486c7c2a7f6cf2f06907
ariel <arielchoum@gmail.com>
2021-10-12 23:02:52 -0400

    implement Item class and tests

--- src/main/java/Entities/Item.java
+++ src/main/java/Entities/Item.java
@@ -1,8 +1,32 @@
 package main.java.Entities;
 
 import main.java.EntityInterfaces.ItemImpl;
+import main.java.Entities.Ingredient;
 
 public class Item implements ItemImpl {
     private Ingredient ingredient;
     private int quantity;
+    private Boolean optional = false;
+
+    public Item(Ingredient ingredient, int quantity) {
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public Ingredient ingredientType() {
+        return null;
+    }
+
+    @Override
+    public int quantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public Boolean optional(Boolean value) {
+        this.optional = value;
+        return this.optional;
+
+    }
 }
--- src/main/java/EntityInterfaces/ItemImpl.java
+++ src/main/java/EntityInterfaces/ItemImpl.java
@@ -1,5 +1,7 @@
 package main.java.EntityInterfaces;
 
 public interface ItemImpl {
-
+    IngredientImpl ingredientType();
+    int quantity();
+    Boolean optional(Boolean value);
 }
