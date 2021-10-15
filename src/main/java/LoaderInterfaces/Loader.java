45099c673032893d76e23b990738a390d10550df
DerekCresswell <derekjcress@gmail.com>
2021-10-15 17:25:41 -0400

    Adds the Row interface and implementations

--- src/main/java/LoaderInterfaces/Loader.java
+++ src/main/java/LoaderInterfaces/Loader.java
@@ -2,5 +2,16 @@ package LoaderInterfaces;
 
 public interface Loader {
 
-}
+	/**
+	 * Returns a new row of data.
+	 *
+	 * @return The next row, or an empty row if there are no more rows.
+	 */
+	Row readRow();
 
+	/**
+	 * Resets the loader to read from the start of source data.
+	 */
+	void resetReader();
+
+}
---
+++ src/main/java/LoaderInterfaces/Row.java
@@ -0,0 +1,29 @@
+package LoaderInterfaces;
+
+public interface Row {
+
+	/**
+	 * The type of entity contained within this row.
+	 *
+	 * @return the string name of the type.
+	 */
+	String type();
+
+	/**
+	 * Determines whether the row contains any data.
+	 *
+	 * @return whether the row is empty.
+	 */
+	boolean empty();
+
+	/**
+	 * Retrieves the given attribute as a given type.
+	 *
+	 * @param attribute The name of the attribute
+	 * @param type The class type for the attribute
+	 *
+	 * @return The attribute cast as type
+	 */
+	<T> T get(String attribute, Class<? extends T> type);
+
+}
