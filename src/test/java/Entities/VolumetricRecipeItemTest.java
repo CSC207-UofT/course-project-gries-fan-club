1301f7f866a44d01582dc3744b9ad70d8773b4aa
ezrar <ezra.robensparadise@mail.utoronto.ca>
2021-10-21 14:21:38 -0400

    Described the RecipeItem interface and converted the RecipeItem interface to an abstract class.
    refactored the abtract quantity to amount
    created the VolumetricRecipe item
    created the QuantityRecipeItem
    created the display method in the recipe  item interface
    implemented the display method in the VolumetricRecipe and the QuantityRecipeItem

---
+++ src/test/java/Entities/VolumetricRecipeItemTest.java
@@ -0,0 +1,21 @@
+package Entities;
+
+import Entities.Implementations.IngredientImpl;
+import Entities.Implementations.RecipeItemImpl;
+import Entities.VolumetricRecipeItem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+
+public class VolumetricRecipeItemTest {
+
+    @Test
+    public void testDisplay() {
+        IngredientImpl ingredient = new IngredientImpl("flour", Collections.emptyList());
+        VolumetricRecipeItem volumeitem = new VolumetricRecipeItem(ingredient, 100, false);
+
+        Assertions.assertEquals("100 ml of flour", volumeitem.display());
+
+    }
+
+}
