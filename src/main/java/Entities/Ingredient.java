bfc51d4d3252568761af6f244eae9b20b60262df
ariel <arielchoum@gmail.com>
2021-10-22 23:58:10 -0400

    add documentation to entity classes/interfaces

--- src/main/java/Entities/Ingredient.java
+++ src/main/java/Entities/Ingredient.java
@@ -5,18 +5,21 @@ import java.util.List;
 public interface Ingredient {
     /**
      * Return the List of Tags for this ingredient
+     *
      * @return The list of tags for the ingredient
      **/
     List<Tag> tags();
 
     /**
      * Return whether the ingredient has the given tag
+     *
      * @return Does the ingredient have the given tag?
      **/
     boolean has(Tag tag);
 
     /**
      * Return the ingredients name
+     *
      * @return the Ingredient name.
      **/
     String name();
--- src/main/java/Entities/RecipeItem.java
+++ src/main/java/Entities/RecipeItem.java
@@ -1,7 +1,24 @@
 package Entities;
 
 public interface RecipeItem {
+    /**
+     * Return the ingredient of the RecipeItem
+     *
+     * @return the ingredient object
+     **/
     Ingredient ingredient();
+
+    /**
+     * Return the quantity of the Ingredient
+     *
+     * @return int representing the quantity
+     **/
     int quantity();
+
+    /**
+     * Return whether or not RecipeItem is optional in recipe
+     *
+     * @return boolean representing if RecipeItem is optional
+     **/
     boolean optional();
 }
