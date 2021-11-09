8054e357fbccfa8c11e8726c2a485edc9efa3448
Gerd Bizi <gerd.bizi@mail.utoronto.ca>
2021-11-08 19:11:17 -0500

    Changed the find methods into more descriptive names. Could be changed later in accordance to overloading. Began re-implementing Storage Interfaces to extend the main storage interface.

--- src/main/java/Storages/Implementations/AbstractStorage.java
+++ src/main/java/Storages/Implementations/AbstractStorage.java
@@ -77,16 +77,8 @@ public abstract class AbstractStorage<T extends Entity> extends AbstractCollecti
 	 *
 	 * @param id The id of the entity to be checked.
 	 *
-	 * @return Whether the entity was removed.
+	 * @return Whether or not the entity was removed.
 	 */
-	public boolean remove(UUID id) throws NoSuchEntity {
-		T entity = this.entities.remove(id.toString());
-
-		if (entity == null) {
-			throw new NoSuchEntity(id);
-		}
-
-		return true;
-	}
+	public boolean remove(UUID id) { return this.entities.remove(id.toString()) != null; }
 
 }
--- src/main/java/Storages/Implementations/RecipeStorageImpl.java
+++ src/main/java/Storages/Implementations/RecipeStorageImpl.java
@@ -1,37 +1,55 @@
 package Storages.Implementations;
 
-import Entities.Ingredient;
-import Entities.Recipe;
-import Entities.Tag;
+import Entities.*;
+import Matchers.Implementations.TagMatcher;
+import Storages.IngredientStorage;
 import Storages.RecipeStorage;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class RecipeStorageImpl extends AbstractStorage<Recipe> implements RecipeStorage {
-
 	@Override
 	public String type() {
 		return "recipe";
 	}
 
 	@Override
-	public Collection<Recipe> recipes() {
-		return null;
+	public Collection<Recipe> recipes() { return this.entities.values(); }
+
+	@Override
+	public Collection<Recipe> findByName(String name) {
+		//Regexes to be added later
+		Collection<Recipe> found = new ArrayList<>();
+		for (Recipe recipe : this.recipes()) {
+			if (recipe.name().contains(name))
+				found.add(recipe);
+		}
+		return found;
 	}
 
 	@Override
-	public Collection<Recipe> find(String name) {
-		return null;
+	public Collection<Recipe> findByTags(Collection<Tag> tags) {
+		Collection<Recipe> found = new ArrayList<>();
+		TagMatcher t = new TagMatcher((List<Tag>) tags); //Use a tag matcher
+		for (Recipe recipe : this.recipes()) {
+			if (t.matches(recipe))
+				found.add(recipe);
+		}
+		return found;
 	}
 
 	@Override
-	public Collection<Recipe> find(Collection<Tag> tags) {
-		return null;
-	}
-
-	@Override
-	public Collection<Recipe> find(List<Ingredient> ingredients) {
-		return null;
+	public Collection<Recipe> findByIngredients(List<Ingredient> ingredients) {
+		Collection<Recipe> found = new ArrayList<>();
+		for (Recipe recipe : this.recipes()) {
+			List<Ingredient> ingredientList = new ArrayList<>();
+			for (RecipeItem item : recipe.items())
+				ingredientList.add(item.ingredient());
+			if (ingredientList.contains(ingredients))
+				found.add(recipe);
+		}
+		return found;
 	}
 }
