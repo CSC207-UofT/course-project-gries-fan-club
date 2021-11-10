ba38b03f8a8d4bb38021e2862887f97057a1fef9
Gerd Bizi <gerd.bizi@mail.utoronto.ca>
2021-11-10 14:39:16 -0500

    Added tags to recipe and its impl. This makes it easier to access them. Made a helper private in that class. Made a Matcher interface. TagMatcher and IngredientMatcher implement it.

---
+++ src/main/java/Matchers/Implementations/TagMatcher.java
@@ -0,0 +1,52 @@
+package Matchers.Implementations;
+
+import Entities.Recipe;
+import Entities.RecipeItem;
+import Entities.Tag;
+import Matchers.Matcher;
+import Storages.Implementations.RecipeStorageImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Match recipes based on tags
+ */
+public class TagMatcher implements Matcher<Recipe> {
+    List<Tag> tags;
+
+    public TagMatcher (List<Tag> tags) { this.tags = tags; }
+
+    /**
+     * Checks if item/ingredient contains any of the tags in the matcher/
+     * returns TRUE if the recipe contains one of the specified tags.
+     */
+    @Override
+    public boolean matches(Recipe recipe) {
+        List<Tag> recipeTags = recipe.tags();
+        for (Tag tag: this.tags)
+            if (recipeTags.contains(tag))
+                    return true;
+        return false;
+    }
+
+    /**
+     * Returns the percentage of specified tags that a recipe has.
+     */
+    public double floatMatch(Recipe recipe) {
+        List<Tag> recipeTags = recipe.tags();
+        int counter = 0;
+        for (Tag tag: this.tags)
+            if (recipeTags.contains(tag))
+                counter++;
+        return (double) counter/this.tags.size();
+    }
+
+    public List<Recipe> allMatches(List<Recipe> recipes) {
+        List<Recipe> matchedRecipes = new ArrayList<>();
+        for (Recipe recipe : recipes)
+            if (matches(recipe))
+                matchedRecipes.add(recipe);
+        return matchedRecipes;
+    }
+}
--- src/main/java/Matchers/Matcher.java
+++ src/main/java/Matchers/Matcher.java
@@ -3,19 +3,18 @@ import Entities.Entity;
 import Entities.Recipe;
 import Storages.Implementations.RecipeStorageImpl;
 
+import java.util.List;
+
 // Matches ingredients with recipes
-public interface TagMatcher<T extends Entity> {
+public interface Matcher<T extends Entity> {
     /** Returns whether or not the specified entity contains all of the tags.
      *
      * @param entity
      * @return whether or not the entity has all the tags..
      */
-    boolean binaryMatch(T entity);
+    boolean matches(T entity);
 
-    /** Returns a decimal percentage of how many of the tags a certain entity has.
-     *
-     * @param entity
-     * @return a decimal percentage of the tags that an entity has.
-     */
     double floatMatch(T entity);
+
+    List<T>  allMatches(List<T> entity);
 }
