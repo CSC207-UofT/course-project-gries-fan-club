5b1ddabd76d5758808ca26d1b1f003741193ac6d
Ayush Sahi <ayushsahi10@gmail.com>
2021-11-12 00:18:33 -0500

    initial commit combining the front end android code and the pre-existing repository. no connections between the back end and front end have been made, file structure is messy and needs to be fixed, need to remove redudent files as well. code runs but is unstable on emulator

---
+++ app/src/main/java/com/example/jchef/InstructionsActivity.java
@@ -0,0 +1,25 @@
+package com.example.jchef;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructionsActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("hi");
+    }
+}
---
+++ app/src/main/java/com/example/jchef/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jchef;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendButton (View view) {
+        //
+    }
+
+
+}
