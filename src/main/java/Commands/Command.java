84dd2c06bd7f9a3ac565d78fb131029d24d08614
ariel <arielchoum@gmail.com>
2021-11-13 19:30:17 -0500

    Make command interface extend Map

--- src/main/java/Commands/Command.java
+++ src/main/java/Commands/Command.java
@@ -2,10 +2,5 @@ package Commands;
 
 import java.util.Map;
 
-public interface Command {
-    /**
-     * Takes in a string and updates the "Tags" key in the data to the value of the rest of the string
-     * @param tagsString string in the format "Tags Tag1, Tag2, Tag3"
-     */
-    void add_tags(String tagsString);
+public interface Command extends Map<String, String> {
 }
