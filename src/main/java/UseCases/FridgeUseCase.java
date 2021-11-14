896af5f08ab9b4186c5eb5499358d8ae38e59711
ezrar <ezra.robensparadise@mail.utoronto.ca>
2021-11-14 11:38:32 -0500

    Refactored to FridgeUseCase use case class

---
+++ src/main/java/UseCases/FridgeUseCase.java
@@ -0,0 +1,10 @@
+package UseCases;
+
+import Commands.Command;
+
+public class FridgeUseCase implements UseCase {
+    @Override
+    public ResponseImpl run(Command command) {
+        return null;
+    }
+}
