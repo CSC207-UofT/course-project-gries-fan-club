38b6249aa98607c6465e0e3435a504b448448ec7
pritheeroy <prithee.roy26@mail.utoronto.ca>
2021-12-02 03:26:56 -0500

    GroceryUseCaseTest Added

---
+++ app/src/test/java/UseCases/GroceryUseCaseTest.java
@@ -0,0 +1,21 @@
+package UseCases;
+
+import Commands.Implementations.CommandImpl;
+import Entities.Implementations.IngredientImpl;
+import Storages.Implementations.IngredientStorageImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+public class GroceryUseCaseTest {
+    @Test
+    public void testListRecipeItems() {
+
+    }
+
+    @Test
+    public void testImportToFridge() {
+
+    }
+}
