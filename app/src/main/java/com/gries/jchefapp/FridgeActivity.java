29d9c87d2e95f420b6a7a169e9b80bf35d7963e4
ayushsahi <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>
2021-12-05 18:08:49 -0500

    added entry points for functions

--- app/src/main/java/com/gries/jchefapp/FridgeActivity.java
+++ app/src/main/java/com/gries/jchefapp/FridgeActivity.java
@@ -1,14 +1,48 @@
 package com.gries.jchefapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.jchefapp.R;
 
 public class FridgeActivity extends AppCompatActivity {
 
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge);
+
+        button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code here
+            }
+        });
+
+        button2 = (Button)findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code here
+            }
+        });
+
+        button3 = (Button)findViewById(R.id.button4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FridgeActivity.this, MainActivity.class));
+
+            }
+        });
+
+
     }
 }
