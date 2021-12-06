367bd915007d30f5e53bf6fcafe9e6fee52582d2
ayushsahi <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>
2021-12-06 17:34:55 -0500

    ccc

--- app/src/main/java/com/gries/jchefapp/FridgeDisplayActivity.java
+++ app/src/main/java/com/gries/jchefapp/FridgeDisplayActivity.java
@@ -1,14 +1,26 @@
 package com.gries.jchefapp;
 
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.jchefapp.R;
 
+import java.util.ArrayList;
+
 public class FridgeDisplayActivity extends AppCompatActivity {
+
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge_display);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Banana");
     }
 }
