4a95ea3d9cf9517d85b35630f8c943f13e559210
ayushsahi <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>
2021-12-06 17:47:28 -0500

    eee

--- app/src/main/java/com/gries/jchefapp/RecipeActivity.java
+++ app/src/main/java/com/gries/jchefapp/RecipeActivity.java
@@ -1,14 +1,29 @@
 package com.gries.jchefapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.jchefapp.R;
 
+import java.util.ArrayList;
+
 public class RecipeActivity extends AppCompatActivity {
+
+    ListView listView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        listView2 = (ListView) findViewById(R.id.listview2);
+
+        ArrayList<String> arrayList2 = new ArrayList<>();
+
+        arrayList2.add("Lasagna");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList2);
+        listView2.setAdapter(arrayAdapter);
     }
 }
