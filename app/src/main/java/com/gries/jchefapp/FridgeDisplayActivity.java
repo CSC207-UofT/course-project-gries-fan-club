06f80933737667a151d4ae59cb8d210090cfd04b
ayushsahi <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>
2021-12-06 17:42:24 -0500

    ddd

--- app/src/main/java/com/gries/jchefapp/FridgeDisplayActivity.java
+++ app/src/main/java/com/gries/jchefapp/FridgeDisplayActivity.java
@@ -1,49 +1,31 @@
 package com.gries.jchefapp;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.jchefapp.R;
 
-public class FridgeDisplayActivity extends AppCompatActivity {
-    private Button addIngredientButton;
-    private Button removeIngredientButton;
+import java.util.ArrayList;
 
-    EditText mEdit;
+public class FridgeDisplayActivity extends AppCompatActivity {
+
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge_display);
 
-        // Add ingredient
-        addIngredientButton = (Button)findViewById(R.id.button6);
-        addIngredientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FridgeDisplayActivity.this, "Ingredient Added", Toast.LENGTH_LONG).show();
-                mEdit   = (EditText)findViewById(R.id.editTextTextPersonName2);
-                // this is the input given to us that we will pass onto the controller
-                mEdit.getText().toString();
-                System.out.println(mEdit.getText().toString());
+        listView = (ListView) findViewById(R.id.listview);
 
-            }
-        });
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        // Remove Ingredient
-        removeIngredientButton = (Button)findViewById(R.id.button9);
-        removeIngredientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FridgeDisplayActivity.this, "Ingredient Removed", Toast.LENGTH_LONG).show();
-                mEdit   = (EditText)findViewById(R.id.editTextTextPersonName2);
-                // this is the input given to us that we will pass onto the controller
-                mEdit.getText().toString();
-                System.out.println(mEdit.getText().toString());
-            }
-        });
+        arrayList.add("Banana");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
