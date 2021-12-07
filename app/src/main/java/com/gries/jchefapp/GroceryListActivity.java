fdba9e64376cd69c9a5a7e9b6e02d74c1d1a90b6
ayushsahi <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>
2021-12-07 02:45:34 -0500

    grocery list added

--- app/src/main/java/com/gries/jchefapp/GroceryListActivity.java
+++ app/src/main/java/com/gries/jchefapp/GroceryListActivity.java
@@ -1,13 +1,34 @@
 package com.gries.jchefapp;
 
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.jchefapp.R;
+
+import java.util.ArrayList;
 
 public class GroceryListActivity extends AppCompatActivity {
+
+//    private Button addIngredientButton2;
+//    private Button removeIngredientButton2;
+//    EditText mEdit;
+    ListView listView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
+
+        listView2 = (ListView) findViewById(R.id.listview2);
+        ArrayList<String> arrayList2 = new ArrayList<>();
+
+        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList2);
+        listView2.setAdapter(arrayAdapter2);
+
+        arrayList2.add("Banana");
+
     }
 }
--- app/src/main/java/com/gries/jchefapp/MainActivity.java
+++ app/src/main/java/com/gries/jchefapp/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InstructionsActivity.class));
+                startActivity(new Intent(MainActivity.this, GroceryListActivity.class));
 
             }
         });
