311e220f542c400c106c7f6825d1cd94d40c2c7e
pritheeroy <prithee.roy26@mail.utoronto.ca>
2021-12-07 04:22:00 -0500

    Added COMMENTS

--- app/src/main/java/com/gries/jchefapp/GroceryListActivity.java
+++ app/src/main/java/com/gries/jchefapp/GroceryListActivity.java
@@ -1,9 +1,9 @@
 package com.gries.jchefapp;
 
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.jchefapp.R;
@@ -12,9 +12,10 @@ import java.util.ArrayList;
 
 public class GroceryListActivity extends AppCompatActivity {
 
-//    private Button addIngredientButton2;
-//    private Button removeIngredientButton2;
-//    EditText mEdit;
+    private Button addIngredientButton;
+    private Button importToFridgeButton;
+
+    EditText mEdit;
     ListView listView2;
 
 
@@ -24,12 +25,65 @@ public class GroceryListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grocery_list);
 
         listView2 = (ListView) findViewById(R.id.listview2);
-        ArrayList<String> arrayList2 = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList2);
-        listView2.setAdapter(arrayAdapter2);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView2.setAdapter(arrayAdapter);
 
-        arrayList2.add("Banana");
+//        arrayList2.add("Banana");
+        addIngredientButton = (Button) findViewById(R.id.button2);
+        addIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GroceryListActivity.this, "IngredientAdded", Toast.LENGTH_LONG).show();
+                mEdit = (EditText) findViewById(R.id.editTextTextPersonName2);
+                // this is the input given to us that we will pass onto the controller
+                mEdit.getText().toString();
+                arrayList.add(mEdit.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                System.out.println(mEdit.getText().toString());
 
+            }
+        });
+
+        // ADD BUTTON FOR THIS
+        importToFridgeButton = (Button) findViewById(R.id.button2);
+        importToFridgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GroceryListActivity.this, "Ingredient Imported to Fridge", Toast.LENGTH_LONG).show();
+                mEdit = (EditText) findViewById(R.id.editTextTextPersonName2);
+                // this is the input given to us that we will pass onto the controller
+                mEdit.getText().toString();
+                // ADD TO FRIDGE
+                arrayList.remove(mEdit.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                System.out.println(mEdit.getText().toString());
+
+            }
+        });
+
+        listView2.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int which_item = position;
+                new AlertDialog.Builder(GroceryListActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this ingredient?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                arrayList.remove(which_item);
+                                // !!!!!!!!!WHEN REMOVED FROM GROCERY. ITEM GETS ADDED TO FRIDGE!!!!!!!!
+                                arrayAdapter.notifyDataSetChanged();
+                                Toast.makeText(GroceryListActivity.this, "Ingredient Removed", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/gries/jchefapp/RecipeActivity.java
+++ app/src/main/java/com/gries/jchefapp/RecipeActivity.java
@@ -1,6 +1,9 @@
 package com.gries.jchefapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +23,17 @@ public class RecipeActivity extends AppCompatActivity {
         listView2 = (ListView) findViewById(R.id.listview2);
 
         ArrayList<String> arrayList2 = new ArrayList<>();
-
         arrayList2.add("Lasagna");
-
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList2);
         listView2.setAdapter(arrayAdapter);
+
+        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(view.getContext(), RecipeDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
