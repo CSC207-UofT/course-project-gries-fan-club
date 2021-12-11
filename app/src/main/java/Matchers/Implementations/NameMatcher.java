24c37e541c498263f221fafaf83bb9be4d6650db
Gerd Bizi <gerd.bizi@mail.utoronto.ca>
2021-12-10 23:29:44 -0500

    Added return types to method headers (for some reason, was not being checked)

--- app/src/main/java/Matchers/Implementations/NameMatcher.java
+++ app/src/main/java/Matchers/Implementations/NameMatcher.java
@@ -8,12 +8,12 @@ public class NameMatcher extends AbstractMatcher {
 
     public NameMatcher(String name) { this.name = name; }
 
-    public matches(Recipe recipe) { return recipe.name() == name; }
+    public boolean matches(Recipe recipe) { return recipe.name() == name; }
 
     /**
      * Use Levenshtein distance to compute difference in strings
      */
-    public floatMatch(Recipe recipe) {
+    public double floatMatch(Recipe recipe) {
         int levDist = StringUtils.getLevenshteinDistance(recipe.name(), name);
         double percentMatch = (double) levDist/recipe.name();
         return (double)1.0 - percentMatch;
